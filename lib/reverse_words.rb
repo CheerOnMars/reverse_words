c2c3ea2a3b2a06dec8dbfbbe08a427f784edd4c0
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-04-12 03:18:13 -0700

    update method

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,20 @@
-# A method to reverse each word in a sentence, in place.
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  if my_words == nil || my_words.length == 0
+    return nil
+  else
+    letter_num = my_words.length
+    reverse_words = my_words
+    counter = 0
+    rev_counter = (letter_num-1)
+    replacement_letter = "a"
+    while counter < rev_counter
+      replacement_letter = my_words[counter]
+      my_words[counter] = my_words[rev_counter]
+      my_words[rev_counter] = replacement_letter
+       counter += 1
+       rev_counter -= 1
+    end
+  end
+  return reverse_words
 end
